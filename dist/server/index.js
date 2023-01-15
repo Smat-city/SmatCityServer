8aa5da9813c08d4295f94951d395356cc31bc10f
Fortune Ehis <ehiscodefox@gmail.com>
2023-01-15 15:52:24 +0100

    fix: Fix server start errors

--- dist/server/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HTTPServer = void 0;
-var http_1 = require("./http");
-Object.defineProperty(exports, "HTTPServer", { enumerable: true, get: function () { return __importDefault(http_1).default; } });
-//# sourceMappingURL=index.js.map
